ccc89a37612b2a43142d2cfc3f81284db77f8ac2
Marie Klevedal <klevdal@ed3354-04.ed3354.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2016-05-11 09:31:45 +0200

    Small changes in jlc and doc

--- dir/test.jl
+++ dir/test.jl
@@ -1,6 +1,9 @@
 int main(){
-    double x = 2.0;
-    boolean b = x == 2.0;
-    boolean a = !b;
-    return 1;
+    printString("hellooo");
+    printString("world");
+    printInt(3+5);
+    printDouble(3.5);
+    int x = readInt();
+    double y = readDouble();
+    return x;
 }
